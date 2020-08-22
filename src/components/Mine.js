bf5bed09cc24d5cfce07d0605c7b539cffb02991
georgemmp <machadogeorge.p@gmail.com>
2020-08-22 16:21:06 -0300

    Initial struct of mine component

---
+++ src/components/Mine.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.coreMine} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
